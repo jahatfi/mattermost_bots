aadff15510894f48b09bf569ce8bbdf5db609793
James Hatfield <jahatfi@g.clemson.edu>
2022-01-09 22:01:41 -0500

    added delay option

--- post_as_bot.py
+++ post_as_bot.py
@@ -66,18 +66,23 @@ def main(parser):
         print("URL: " + channel_by_name_url)
         sys.exit(1)
 
+    delay_seconds = 0
+    if args.delay:
+        try:
+            target_time = datetime.strptime(args.delay, "%m/%d/%Y %H:%M")
+        except ValueError as e:
+            print("Invalid time format, time format must be MM/DD/YYYY: HH:MM")
+            sys.exit(1)
+        delay = target_time - datetime.now()
+        print(f"target _time: {type(target_time)}, delay: {type(delay)}")
+        print(f"I need to sleep until {target_time}, that's {delay}, or {delay.seconds} seconds")
+        delay_seconds = delay.seconds
+
     # Post to channel with provided message
     if args.live_run:
-        if args.delay:
-            try:
-                target_time = datetime.strptime(args.delay, "%m/%d/%Y %H:%M")
-            except ValueError as e:
-                print("Invalid time format, time format must be MM/DD/YYYY: HH:MM")
-                sys.exit(1)
-            delay = target_time - datetime.now()
-            print(f"target _time: {type(target_time)}, delay: {type(delay)}")
-            print(f"I need to sleep until {target_time}, that's {delay}, or {delay.seconds} seconds")
-            time.sleep(delay.seconds)
+        print("Sleeping...")
+        time.sleep(delay.seconds)
+
 
         print(f"Posting to {args.channel}\nMessage: '{message}'")
         
@@ -133,5 +138,5 @@ if __name__ == "__main__":
                         type=str,
                         help="Delay until date/time in format: MM/DD/YYYY: HH:MM"
                         )  
-                                                                                                                         
+
     all_users = main(parser)        
