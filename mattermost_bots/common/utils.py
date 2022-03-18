99211b29ee609d21f7cc530c39aa2c6b1a1f3931
James Hatfield <jahatfi@g.clemson.edu>
2022-03-18 00:55:25 -0400

    Added google sheets feature

--- mattermost_bots/common/utils.py
+++ mattermost_bots/common/utils.py
@@ -16,10 +16,10 @@ pd.set_option('display.width', 1000)
 # ==============================================================================
 def parse_creds_from_file(authentication_info):
     """
-    Parse file with (one per line): 
-    (1 server url, 
-    (2 team id, 
-    (3 auth token, 
+    Parse file with (one per line):
+    (1 server url,
+    (2 team id,
+    (3 auth token,
     (4 bot name
     Return them all in a tuple
     """
@@ -29,21 +29,24 @@ def parse_creds_from_file(authentication_info):
         team_id = creds_file.readline().split('=')[1].strip()
         token   = creds_file.readline().split('=')[1].strip()
         bot_name = creds_file.readline().split('=')[1].strip()
-
-        return (url, team_id, token, bot_name)
+        try:
+            sheet_id = creds_file.readline().split('=')[1].strip()
+        except IndexError:
+            return (url, team_id, token, bot_name)
+        return (url, team_id, token, bot_name, sheet_id)
 # ==============================================================================
 def rename_bot(base_url, old_bot_name, old_bot_id, new_bot_name, headers):
     """
     Rename bot to new name
     """
     payload = {}
-    payload["username"] = new_bot_name   
+    payload["username"] = new_bot_name
     payload["display_name"] = new_bot_name
-    resp = requests.put(base_url+"api/v4/bots/"+old_bot_id, 
+    resp = requests.put(base_url+"api/v4/bots/"+old_bot_id,
                             headers=headers,
                             data=json.dumps(payload))
 
-    
+
     if resp.status_code < 200 or resp.status_code > 299:
         print(f"Failed to rename {old_bot_name} bot to {new_bot_name}")
         print(resp)
@@ -67,20 +70,20 @@ def update_bot_icon(base_url, bot_name, bot_id, new_image_file, headers):
 
     new_headers = {}
     new_headers['Authorization'] = headers['Authorization']
-    resp = requests.post(base_url+"api/v4/bots/"+bot_id+'/'+"icon", 
+    resp = requests.post(base_url+"api/v4/bots/"+bot_id+'/'+"icon",
                          headers=new_headers,
                          files=files
             )
-    
+
     if resp.status_code < 200 or resp.status_code > 299:
         print(f"Failed to update icon for {bot_name}")
-        pprint.pprint(json.loads(resp.text))    
+        pprint.pprint(json.loads(resp.text))
         return False
     else:
         # TODO This doesn't seem to work!
         print("Updating icon currently doesn't work even though server responded with 200/OK:")
         #print(f"Updated image for {bot_name} to {new_image_file}")
-        pprint.pprint(json.loads(resp.text))    
+        pprint.pprint(json.loads(resp.text))
 
     return True
 # ==============================================================================
@@ -91,7 +94,7 @@ def return_computed_delay(target_time):
     try:
         target_time = datetime.strptime(target_time, "%m/%d/%Y %H:%M")
     except ValueError:
-        print("Invalid time format, time format must be MM/DD/YYYY: HH:MM")
+        print("Invalid time format, time format must be MM/DD/YYYY HH:MM")
         sys.exit(1)
     if target_time < datetime.now():
         print(f"{target_time} in the past!")
@@ -111,7 +114,7 @@ def create_dm_channel(base_url, bot_id, bot_name, user_id, header):
 
     if channel_info.status_code < 200 or channel_info.status_code > 299:
         print(f"Failed to update icon for {bot_name}")
-        pprint.pprint(json.loads(channel_info.text))    
+        pprint.pprint(json.loads(channel_info.text))
         sys.exit(-1)
 
     channel_info = json.loads(channel_info.text)
@@ -134,14 +137,14 @@ def get_channels(base_url, headers):
     page = 0
     all_channels = []
     channel_url = base_url+"api/v4/channels"
-    
+
     print(f"Retrieving info for channels, starting with all channels")
     while True:
         headers["page"] = page
         resp = requests.get(channel_url, headers=headers)
         #print(resp.text)
         new_dict = json.loads(resp.text)
-        
+
         all_channels += new_dict
         all_channels = pd.DataFrame(all_channels)
         cols_to_drop = all_channels.columns
@@ -153,14 +156,14 @@ def get_channels(base_url, headers):
         if all_channels.empty:
             print("Failed")
         pprint.pprint(all_channels)
-        return all_channels    
+        return all_channels
 # ==============================================================================
 def get_users(users_url, headers, usernames, channels, results_per_page):
     """
     Returns a Pandas DataFrame of all users on this server, but with only
     a subset of the columns that are useful for identifying the users.
 
-    If both usernames and channels_ids provided, return the intersection of the 
+    If both usernames and channels_ids provided, return the intersection of the
     users in both those channels AND the username list.
     Otherwise, if only a list of usernames OR channels is provided,
     return those users.
@@ -168,13 +171,13 @@ def get_users(users_url, headers, usernames, channels, results_per_page):
     cols_to_keep = ['id', 'username', 'email', 'first_name', 'nickname', 'last_name', 'status']
     page = 0
     all_users = []
-    
+
     if any(channels):
         for _, channel in channels.iterrows():
             channel_name = channel['name']
             channel_id = channel['id']
             while True:
-              
+
                 print(f"Retrieving info for users in channel {channel_name}, page {page} ({results_per_page} entries per page)")
                 resp = requests.get(users_url+f"?page={page}&in_channel={channel_id}", headers=headers)
                 new_dict = json.loads(resp.text)
@@ -185,15 +188,15 @@ def get_users(users_url, headers, usernames, channels, results_per_page):
 
     else:
         while True:
-            
+
             print(f"Retrieving info for users, page {page} ({results_per_page} entries per page)")
             resp = requests.get(users_url+f"?page={page}", headers=headers)
             new_dict = json.loads(resp.text)
-            
+
             all_users += new_dict
             if len(new_dict) < results_per_page:
                 break
-            page += 1        
+            page += 1
 
 
     all_users = pd.DataFrame(all_users)
@@ -224,15 +227,15 @@ def get_bot_info(base_url, bot_id, headers):
 def send_dm(base_url, channel_id, header, message):
     """
     Send dm to the provided channel
-    """    
+    """
     header['Content-type'] = "application/json"
-    dm_info = requests.post(base_url+"api/v4/posts", 
+    dm_info = requests.post(base_url+"api/v4/posts",
                                 headers=header,
                                 data=json.dumps({"channel_id": channel_id, "message":message}))
     dm_info = json.loads(dm_info.text)
     return dm_info
 
-#==============================================================================    
+#==============================================================================
 def search_keyword(args, url, headers, team_id, channels):
     reaction_url = f"{url}api/v4/teams/{team_id}/posts/search"
     results = pd.DataFrame()
@@ -247,14 +250,14 @@ def search_keyword(args, url, headers, team_id, channels):
         for page in range(6):
             print(f"Getting Page {page} of {channels.iloc[channel_index]['name']} posts")
             #headers['page'] = page
-            resp = requests.get(   get_posts_url+f"?page={page}", 
+            resp = requests.get(   get_posts_url+f"?page={page}",
                                     headers=headers,
                                     data=json.dumps(data)
                                 )
             if resp.status_code < 200 or resp.status_code > 299:
                 print("Error")
-                print(f"URL was '{reaction_url}'.  See the problem?")  
-                print(resp.text)          
+                print(f"URL was '{reaction_url}'.  See the problem?")
+                print(resp.text)
 
             elif resp.text:
                 posts = json.loads(resp.text)
@@ -272,9 +275,9 @@ def search_keyword(args, url, headers, team_id, channels):
                 #headers["before"] = posts["prev_post_id"]
                 #print(posts["prev_post_id"])
     results = results.reset_index(drop=True)
-    #pprint.pprint(results)    
+    #pprint.pprint(results)
     return results
-#==============================================================================    
+#==============================================================================
 def read_usernames(username_file):
 
     if username_file:
